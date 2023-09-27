231b9420521146429b5b78f4fb661ec18d25a521
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-09-27 11:36:12 -0300

    test: endpoint para buscar resultado de sessão

--- api/src/test/java/com/db/api/services/SessaoServiceTest.java
+++ api/src/test/java/com/db/api/services/SessaoServiceTest.java
@@ -1,9 +1,11 @@
 package com.db.api.services;
 
+import com.db.api.dtos.response.SessaoResponse;
+import com.db.api.enums.ResultadoSessao;
+import com.db.api.enums.VotoEnum;
 import com.db.api.exceptions.RegistroNaoEncontradoException;
 import com.db.api.models.Pauta;
 import com.db.api.models.Sessao;
-import com.db.api.repositories.PautaRepository;
 import com.db.api.repositories.SessaoRepository;
 import com.db.api.repositories.VotoRepository;
 import com.db.api.stubs.PautaStub;
@@ -16,6 +18,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -28,11 +31,13 @@ class SessaoServiceTest {
 
     @Mock
     private PautaService pautaService;
+    @Mock
+    private VotoRepository votoRepository;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        sessaoService = new SessaoService(sessaoRepository, pautaService);
+        sessaoService = new SessaoService(sessaoRepository, pautaService, votoRepository);
     }
 
     @Test
@@ -54,4 +59,56 @@ class SessaoServiceTest {
 
         verify(sessaoRepository, never()).save(any(Sessao.class));
     }
+
+    @Test
+    @DisplayName("Deve obter resultado de sessão com votos favoráveis")
+    void testObterResultadoSessaoAprovada() {
+        Sessao sessao = SessaoStub.gerarSessaoEncerrada();
+
+        when(sessaoRepository.findById(sessao.getId())).thenReturn(Optional.of(sessao));
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.SIM, sessao.getId())).thenReturn(5);
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.NAO, sessao.getId())).thenReturn(3);
+
+        SessaoResponse response = sessaoService.contabilizarVotos(sessao.getId());
+
+        assertEquals(ResultadoSessao.APROVADA, response.getResultadoSessao());
+    }
+
+    @Test
+    @DisplayName("Deve obter resultado de sessão com votos contrários")
+    void testObterResultadoSessaoReprovada() {
+        Sessao sessao = SessaoStub.gerarSessaoEncerrada();
+
+        when(sessaoRepository.findById(sessao.getId())).thenReturn(Optional.of(sessao));
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.SIM, sessao.getId())).thenReturn(3);
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.NAO, sessao.getId())).thenReturn(5);
+
+        SessaoResponse response = sessaoService.contabilizarVotos(sessao.getId());
+
+        assertEquals(ResultadoSessao.REPROVADA, response.getResultadoSessao());
+    }
+
+    @Test
+    @DisplayName("Deve obter resultado de sessão indefinido em caso de empate")
+    void testObterResultadoSessaoIndefinida() {
+        Sessao sessao = SessaoStub.gerarSessaoEncerrada();
+        when(sessaoRepository.findById(sessao.getId())).thenReturn(Optional.of(sessao));
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.SIM, sessao.getId())).thenReturn(4);
+        when(votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.NAO, sessao.getId())).thenReturn(4);
+
+        SessaoResponse response = sessaoService.contabilizarVotos(sessao.getId());
+
+        assertEquals(ResultadoSessao.INDEFINIDA, response.getResultadoSessao());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção ao obter resultado de sessão não encontrada")
+    void testObterResultadoSessaoNaoEncontrada() {
+        Long sessionId = 1L;
+        when(sessaoRepository.findById(sessionId)).thenReturn(Optional.empty());
+
+        assertThrows(RegistroNaoEncontradoException.class, () -> sessaoService.contabilizarVotos(sessionId));
+    }
+
+
 }
