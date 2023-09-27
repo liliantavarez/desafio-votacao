f644e6c44e6f73697409caea2fdf624d7eaeb7c2
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-09-27 09:15:43 -0300

    test: novo cenário de teste

--- api/src/test/java/com/db/api/controllers/AssociadoControllerTest.java
+++ api/src/test/java/com/db/api/controllers/AssociadoControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
 
 import static com.db.api.SqlProvider.inserirAssociado;
 import static com.db.api.SqlProvider.resetarDB;
@@ -19,6 +18,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = resetarDB)
 class AssociadoControllerTest {
 
     private final String URL = "/api/v1/associados";
@@ -31,7 +31,7 @@ class AssociadoControllerTest {
     }
 
     @Test
-    @DisplayName("Deve cadastrar pauta com sucesso")
+    @DisplayName("Deve registrar um associado com sucesso")
     void deveRegistrarAssociadoComSucesso() {
         RestAssured.given()
                 .contentType(ContentType.JSON)
@@ -52,18 +52,29 @@ class AssociadoControllerTest {
                 .body(AssociadoStub.gerarAssociadoDtoCpfInvalida())
                 .when()
                 .post(URL + "/registrar")
-                .then().log().all()
+                .then()
                 .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value())
                 .body("mensagem", equalTo("Erro de validação"))
                 .body("detalhes", hasItem("número do registro de contribuinte individual brasileiro (CPF) inválido"));
     }
-
+    @Test
+    @DisplayName("Deve retornar um exceção ao tentar registrar associado sem informar seu nome")
+    void testRegistrarAssociadoNomeVazio() {
+        given()
+                .contentType("application/json")
+                .body(AssociadoStub.gerarAssociadoDtoNomeVazio())
+                .when()
+                .post(URL + "/registrar")
+                .then()
+                .statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .body("mensagem", equalTo("Erro de validação"))
+                .body("detalhes", hasItem("Por favor informe o nome do associado!"))
+                .body("detalhes", hasItem("tamanho deve ser entre 3 e 150"));
+    }
     @Test
     @DisplayName("Deve retornar um exceção ao tentar registrar associado com cpf já cadastrado no sistema")
-    @SqlGroup({
-            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = inserirAssociado),
-            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = resetarDB)
-    })
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = resetarDB)
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = inserirAssociado)
     void testRegistrarAssociadoMaisDeUmaVez() {
         given()
                 .contentType("application/json")
