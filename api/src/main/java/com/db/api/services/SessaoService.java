543dbc32a3f6a0cc6e1e6e3dff66a278fafc6346
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-09-28 11:22:56 -0300

    refactor: contando votos direto do service de sessão pra não depender do service de voto

--- api/src/main/java/com/db/api/services/SessaoService.java
+++ api/src/main/java/com/db/api/services/SessaoService.java
@@ -9,13 +9,15 @@ import com.db.api.exceptions.SessaoEncerradaException;
 import com.db.api.models.Pauta;
 import com.db.api.models.Sessao;
 import com.db.api.repositories.SessaoRepository;
-import com.db.api.repositories.VotoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -23,7 +25,8 @@ public class SessaoService {
 
     private final SessaoRepository sessaoRepository;
     private final PautaService pautaService;
-    private final VotoRepository votoRepository;
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Transactional
     public Sessao iniciarSessaoVotacao(String pautaTitulo, LocalDateTime dataEncerramento) {
@@ -53,8 +56,7 @@ public class SessaoService {
 
     @Transactional
     public Sessao validarSessao(long sessaoId) {
-        Sessao sessao = sessaoRepository.findById(sessaoId)
-                .orElseThrow(() -> new RegistroNaoEncontradoException("Sessão não encontrada."));
+        Sessao sessao = buscarSessaoPorID(sessaoId);
 
         if (sessao.getDataEncerramento().isBefore(LocalDateTime.now()) && sessao.getStatusSessao() == StatusSessao.ABERTA) {
             encerrarSessao(sessao);
@@ -72,22 +74,37 @@ public class SessaoService {
         sessao.setStatusSessao(StatusSessao.ENCERRADA);
         sessaoRepository.save(sessao);
     }
+
     @Transactional
-    public SessaoResponse contabilizarVotos(Long id) {
-        Sessao sessao = buscarSessaoPorId(id);
-        int votosSim = votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.SIM, id);
-        int votosNao = votoRepository.countVotoByVotoEnumAndAndSessaoId(VotoEnum.NAO, id);
+    public SessaoResponse contabilizarVotos(Long sessaoId) {
+        Sessao sessao = buscarSessaoPorID(sessaoId);
+        if (sessao == null) {
+            throw new RegistroNaoEncontradoException("");
+        }
+        String querySQLContarVotos = "SELECT v.votoEnum, COUNT(v) FROM Voto v WHERE v.sessao.id = :sessaoId GROUP BY v.votoEnum";
+        TypedQuery<Object[]> query = entityManager.createQuery(querySQLContarVotos, Object[].class);
+        query.setParameter("sessaoId", sessaoId);
+        List<Object[]> results = query.getResultList();
+
+        long votosSim = 0;
+        long votosNao = 0;
+        for (Object[] result : results) {
+            VotoEnum votoEnum = (VotoEnum) result[0];
+            long count = (long) result[1];
+
+            if (votoEnum == VotoEnum.SIM) {
+                votosSim = count;
+            } else if (votoEnum == VotoEnum.NAO) {
+                votosNao = count;
+            }
+        }
 
         definirResultadoSessao(sessao, votosSim, votosNao);
 
         return new SessaoResponse(sessao.getPauta(), sessao.getDataEncerramento(), sessao.getResultadoSessao());
     }
-    private Sessao buscarSessaoPorId(Long id) {
-        Optional<Sessao> sessaoOptional = sessaoRepository.findById(id);
-        return sessaoOptional.orElseThrow(() -> new RegistroNaoEncontradoException("A sessão requerida não foi encontrada!"));
-    }
 
-    private void definirResultadoSessao(Sessao sessao, int votosSim, int votosNao) {
+    private void definirResultadoSessao(Sessao sessao, Long votosSim, Long votosNao) {
         if (votosSim > votosNao) {
             sessao.setResultadoSessao(ResultadoSessao.APROVADA);
         } else if (votosNao > votosSim) {
@@ -95,6 +112,10 @@ public class SessaoService {
         } else {
             sessao.setResultadoSessao(ResultadoSessao.INDEFINIDA);
         }
+    }
+
+    public Sessao buscarSessaoPorID(Long sessaoId) {
+        return sessaoRepository.findById(sessaoId).orElseThrow(() -> new RegistroNaoEncontradoException("Sessão não encontrada."));
     }
 }
 
