9f22cca922b2f26d19347a0f045e5b25640295d9
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-10-10 07:48:09 -0300

    feat: incluindo endpoints de busca na documentação

--- src/main/java/com/db/api/exceptions/RegistroNaoEncontradoException.java
+++ src/main/java/com/db/api/exceptions/RegistroNaoEncontradoException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RegistroNaoEncontradoException extends RuntimeException {
     public RegistroNaoEncontradoException(String message) {
-        super(message);;
+        super(message);
     }
 }
