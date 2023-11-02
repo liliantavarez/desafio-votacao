5e1c8cb705cabfd8b01ef8900df14ca28bbc4d30
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-11-02 10:17:29 -0300

    feat: configuração do cors

---
+++ src/main/java/com/db/api/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.db.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "HEAD", "PATCH", "PUT")
+                .allowedOrigins();
+    }
+}
