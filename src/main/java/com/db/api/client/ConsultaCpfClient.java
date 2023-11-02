bcc7d70507f505b32e4c5c23ce687ff15af50353
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-11-02 16:43:18 -0300

    fix: atualizando url da api externa para consulta da situação do cpf

--- src/main/java/com/db/api/client/ConsultaCpfClient.java
+++ src/main/java/com/db/api/client/ConsultaCpfClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ConsultaCpf", url = "${url.api.consulta.cpf}/5ae973d7a997af13f0aaf2bf60e65803/9")
+@FeignClient(name = "ConsultaCpf", url = "${url.api.consulta.cpf}")
 public interface ConsultaCpfClient {
     @GetMapping("/{cpf}")
     ConsultaCPFResponse verificarSituacaoCPF(@PathVariable("cpf") String cpf);
