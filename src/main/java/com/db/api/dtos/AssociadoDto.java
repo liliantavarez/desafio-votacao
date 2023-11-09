052e479ac560ae6b45e8e82b191695f22abb04b1
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-11-09 20:19:33 -0300

    refactor: incluindo id no dto de associado  removendo validações

--- src/main/java/com/db/api/dtos/AssociadoDto.java
+++ src/main/java/com/db/api/dtos/AssociadoDto.java
@@ -2,19 +2,17 @@ package com.db.api.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.br.CPF;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class AssociadoDto {
-    @NotBlank(message = "Por favor informe o nome do associado!")
-    @Size(min = 3, max = 150)
+
+    private long id;
     private String nome;
-    @CPF
     private String cpf;
+
+    public AssociadoDto(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
 }
