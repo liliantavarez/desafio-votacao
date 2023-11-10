ffd381530922341b8d67878a614f6db3e743c135
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-11-10 08:19:06 -0300

    test: stubs para o dto PautaRequest

--- src/test/java/com/db/api/stubs/PautaStub.java
+++ src/test/java/com/db/api/stubs/PautaStub.java
@@ -1,5 +1,6 @@
 package com.db.api.stubs;
 
+import com.db.api.dtos.request.PautaRequest;
 import com.db.api.models.Pauta;
 
 public interface PautaStub {
@@ -17,9 +18,16 @@ public interface PautaStub {
                 .build();
     }
 
-    static Pauta gerarPautaDtoTituloVazio() {
-        return Pauta.builder()
+    static PautaRequest gerarPautaDtoTituloVazio() {
+        return PautaRequest.builder()
                 .titulo(" ")
+                .descricao("Discussão sobre a adição de notificações à aplicação.")
+                .build();
+    }
+
+    static PautaRequest gerarPautaRequest() {
+        return PautaRequest.builder()
+                .titulo("Novas funcionalidades")
                 .descricao("Discussão sobre a adição de notificações à aplicação.")
                 .build();
     }
