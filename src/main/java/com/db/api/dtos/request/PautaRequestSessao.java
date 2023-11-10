ee63dce32f5dfc752cbb7b0c26775ad9096e3352
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-11-10 09:37:53 -0300

    fix: construtor com a anotação @JsonCreator e @JsonProperty para melhorar a desserialização JSON

--- src/main/java/com/db/api/dtos/request/PautaRequestSessao.java
+++ src/main/java/com/db/api/dtos/request/PautaRequestSessao.java
@@ -1,11 +1,22 @@
 package com.db.api.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Builder
+@NoArgsConstructor
 public class PautaRequestSessao {
     @NotBlank(message = "Por favor, informe o titulo da pauta.")
     private String titulo;
+    @JsonCreator
+    public PautaRequestSessao(@JsonProperty("titulo") String titulo) {
+        this.titulo = titulo;
+    }
 }
